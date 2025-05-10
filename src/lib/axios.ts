4cef5b43cc70b1b772589246e82f12be2a4d4a38
renato3x <pereirarenato21@gmail.com>
2025-05-10 01:10:20 -0300

    chore: improve axios global error handler

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,5 +1,6 @@
 import { authService } from '@/services/auth';
 import { ApiErrorResponse } from '@/types/api';
+import { FREE_AUTH_PATHS } from '@/utils';
 import { notifier } from '@/utils/notifier';
 import axios, { isAxiosError } from 'axios';
 
@@ -21,16 +22,20 @@ api.interceptors.response.use(
       return Promise.reject(error);
     }
 
-    if (error.status === 401 && !['/signin', '/signup', '/forgot-password'].includes(location.pathname)) {
+    if (error.response?.status === 401) {
+      if (FREE_AUTH_PATHS.includes(location.pathname)) {
+        return Promise.reject(error);
+      }
+
       await authService.signout();
       notifier.error('Session expired', 'Your current session has expired. Sign in again.');
       return Promise.reject(error);
     }
 
-    const response = error.response!.data;
-    const message = response.errors ? response.errors[0] : response.message;
+    const response = error.response?.data;
+    const message = response?.errors?.[0] || response?.message || 'Unexpected error';
 
-    notifier.error(response.error, message);
+    notifier.error(response?.error || 'Error', message);
     return Promise.reject(error);
   },
 );
